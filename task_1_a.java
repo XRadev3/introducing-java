df9df1828f13ba6fe531aca62f248a0d7c97e0d3
XRadev3 <xradev@abv.bg>
2017-05-19 16:32:56 +0300

    Add files via upload
    Тест

---
+++ task_1_a.java
@@ -0,0 +1,13 @@
+package com.sirma.itt.javacourse.introducing_java;
+
+import java.util.Scanner;
+
+public class task_1_a {
+	Scanner s1 = new Scanner(System.in);
+	Scanner s2 = new Scanner(System.in);
+	System.out.printf("Please enter the first digit: ");
+	int digit1 = s1.nextInt();
+	System.out.printf("Please enter the second digit: ");
+	int digit2 = s2.nextInt();
+	
+}
